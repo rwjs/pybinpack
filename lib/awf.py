c20811ec13bf097d4673a603b146d4cb7590ba9e
rwjs <soapbar2@gmail.com>
2013-05-18 16:22:34 +1000

    Added common.py

--- lib/awf.py
+++ lib/awf.py
@@ -10,29 +10,31 @@ def pack(*items, **kwargs):
     that this heuristic is provably better than Worst Fit, but I have thus far
     been unable to locate anything definitive."""
 
-    key = kwargs.get('key', lambda x:x)
+    key = kwargs.get('key', (lambda x:x, ))
     binsize = kwargs.get('binsize', 800)
     bins = kwargs.get('bins', [[]])
+    dimensions = kwargs.get('dimensions', 1)
 
     bins.sort(key=lambda x:sum(x))
 
     for item in items:
-        size_item = key(item)
-        if size_item > binsize:
-            yield DoesNotFitWarning(item)
-            continue
-        try:
-            assert binsize - sum(bins[1]) >= size_item
-            # IndexError if no bins[1], AssertionError if not fit.
-            bins[1].append(item)
-        except (AssertionError, IndexError):
-            if bins and binsize - sum(bins[0]) >= size_item:
-            # At least one bin exists and the item fits in it
-                bins[0].append(item)
-            else:
-            # Create new bin
-                bins.append([item])
-        bins.sort(key=lambda x:sum(x)) 
-        # TimSort is quite efficient for partially sorted lists..
-    for item in bins:
-        yield item
+        for dim in range(0, dimensions):
+            size_item = key(item)
+            if size_item > binsize:
+                yield DoesNotFitWarning(item)
+                continue
+            try:
+                assert binsize - sum(bins[1]) >= size_item
+                # IndexError if no bins[1], AssertionError if not fit.
+                bins[1].append(item)
+            except (AssertionError, IndexError):
+                if bins and binsize - sum(bins[0]) >= size_item:
+                # At least one bin exists and the item fits in it
+                    bins[0].append(item)
+                else:
+                # Create new bin
+                    bins.append([item])
+            bins.sort(key=lambda x:sum(x)) 
+            # TimSort is quite efficient for partially sorted lists..
+        for b in bins:
+            yield b
